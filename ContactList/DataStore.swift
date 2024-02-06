192b5bdf513a9811b1e5346a2e8a5a718aa6945f
IrinaMuravyova <miomir@yandex.ru>
2024-02-07 04:21:00 +0800

    Added func to Person.swift

--- ContactList/DataStore.swift
+++ ContactList/DataStore.swift
@@ -5,4 +5,25 @@
 //  Created by Irina Muravyeva on 07.02.2024.
 //
 
-import Foundation
+class DataStore {
+    let names: [String] = ["James", "Mary", 
+                           "Robert", "Patricia",
+                           "John", "Jennifer",
+                           "Michael", "Linda",
+                           "David", "Elizabeth"]
+    let surnames: [String] = ["Smith", "Jones", 
+                              "Williams", "Brown",
+                              "Taylor", "Davies",
+                              "Evans", "Thomas",
+                              "Johnson", "Roberts"]
+    let phones: [String] = ["+44 20 7123 4567", "+61 3 7010 4321",
+                            "+61 491 578 888", "+1 212 555 0188",
+                            "+44 20 7946 0990", "+88 017 7129 2198",
+                            "+21 2456 7890", "+141 5123 1234",
+                            "+370 6011 2345", "+61 2 5550 4321"]
+    let emails: [String] = ["pk@zylker.com, info@mail.com",
+                            "lastsamurai@earth.com", "mister@yandex.ru",
+                            "ninetees@brk.ru", "bang_man@google.ru",
+                            "catwoman_on_hunt@ya.ru", "phantom_lady@example.ru",
+                            "urdreamgirl@yourdoor.com", "hotmale@hotmail.com"]
+}
--- ContactList/Person.swift
+++ ContactList/Person.swift
@@ -5,4 +5,32 @@
 //  Created by Irina Muravyeva on 07.02.2024.
 //
 
-import Foundation
+struct Person {
+    var name: String
+    var suranme: String
+    var phone: String
+    var email: String
+    
+    var fullName: String {
+        "\(name) \(suranme)"
+    }
+    
+    init() {
+        name = ""
+        suranme = ""
+        phone = ""
+        email = ""
+    }
+    
+    func addPersonsList(names: [String], surnames: [String], phones: [String], emails: [String]) -> Person {
+        
+        var person: Person = Person()
+        
+        person.name = names.randomElement()!
+        person.suranme = surnames.randomElement()!
+        person.phone = phones.randomElement()!
+        person.email = emails.randomElement()!
+        
+        return person
+    }
+}
