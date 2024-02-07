806f198984152486327dfd3b82f3d701ebe391ab
IrinaMuravyova <miomir@yandex.ru>
2024-02-07 08:02:11 +0800

    Showed details info for person in DetailsView

---
+++ ContactList/PersonDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PersonDetailsViewController.swift
+//  ContactList
+//
+//  Created by Irina Muravyeva on 07.02.2024.
+//
+
+import UIKit
+
+class PersonDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
