30b8e4bf37a9c2d63f0109f335d0c5c8833b389c
IrinaMuravyova <miomir@yandex.ru>
2024-02-12 11:54:29 +0800

    Changed var to let in molel Person

--- ContactList/Models/Person.swift
+++ ContactList/Models/Person.swift
@@ -6,10 +6,10 @@
 //
 
 struct Person {
-    var name: String
-    var surname: String
-    var phone: String
-    var email: String
+    let name: String
+    let surname: String
+    let phone: String
+    let email: String
     
     var fullName: String {
         "\(name) \(surname)"
